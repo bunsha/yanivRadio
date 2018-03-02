dd80eec7f5973f7085c1ec5f908037905e4fc838
bunsha <roman@bunsha.ru>
2018-03-02 14:04:19 +0300

    123

--- original/js/script.js
+++ original/js/script.js
@@ -7,41 +7,34 @@ loadView();
 initPlayEvents();
 
 function initPlayEvents() {
-    $('>div',$ctr).on('click',function() {
+    $('>div', $ctr).on('click', function () {
         boxclick($(this));
     });
-    $('#controls > .topause').on('click',function() {
+    $('#controls > .topause').on('click', function () {
         pause();
     });
 }
 
 function boxclick(obj) {
-    var action = true;
-    if($current) {
-        if(obj[0]==$current[0] && status!='error') {
-            audio.play();
-            action = false;
-        } 
-    }
-    if(action) {
+    if ($current==null ||  $current[0] != obj[0] || status != 'playing') {
         var pos = obj[0].getBoundingClientRect();
         var sid = $(obj).data('sid');
-        var bg = 'url(img/st/'+stations[sid][0]+'.jpg)';
+        var bg = 'url(img/st/' + stations[sid][0] + '.jpg)';
         $('<div class="anim"></div>').css({
-            "top": pos.top+"px",
-            "left": pos.left+"px",
+            "top": pos.top + "px",
+            "left": pos.left + "px",
             "background-image": bg
         }).appendTo('body').animate({
-            top: "20px", 
-            left: "20px", 
+            top: "20px",
+            left: "20px",
             width: "50px",
             height: "50px",
             borderRadius: "25px"
-        },300,function(e){            
-            $('#controls > .station').css('background-image',bg);
+        }, 300, function (e) {
+            $('#controls > .station').css('background-image', bg);
             $(this).remove();
             $current = obj;
-            $('#controls p').text(stations[sid][1]);            
+            $('#controls p').text(stations[sid][1]);
             audio.src = stations[sid][2];
             audio.play();
         });
@@ -49,52 +42,57 @@ function boxclick(obj) {
 }
 
 function pause() {
-    if(status=='playing') {
-        audio.pause();
-    }  
-    else if(status=='pause')   {
-        audio.play();
+    if(status && status != 'error') {
+        if (status == 'pause') {
+            audio.play();
+        }
+        else {
+            audio.pause();
+        }
     }
 }
 
 function loadView() {
-    for(var i=0;i<stations.length;i++) {
-        addBox(i,stations[i][0]);
+    for (var i = 0; i < stations.length; i++) {
+        addBox(i, stations[i][0]);
     }
 }
 
-function addBox(index,img) {
-    $ctr.append('<div id="s'+index+'" data-sid="'+index+'" style="background-image:url(img/st/'+img+'.jpg)">');
+function addBox(index, img) {
+    $ctr.append('<div id="s' + index + '" data-sid="' + index + '" style="background-image:url(img/st/' + img + '.jpg)">');
 }
 
-function initAudioEvents() {    
-    // -- possible events 
-    // https://www.w3schools.com/tags/ref_av_dom.asp          
-    ['error','stalled','loadstart','playing','pause'].map( function(evt) {
-        audio.addEventListener(evt,function (e) {
-            setStatus(e.type);                  
+function initAudioEvents() {
+    //['abort','canplay','canplaythrough','durationchange','emptied','ended','error','loadeddata','loadedmetadata','loadstart','pause','play','playing','progress','ratechange','seeked','seeking','stalled','suspend','timeupdate','volumechange','waiting']
+    ['play', 'loadstart', 'waiting', 'stalled', 'playing', 'pause', 'error']
+        .map(function (evt) {
+            audio.addEventListener(evt, function (e) {
+                eventFire(e.type);
+            });
         });
-    });   
 }
 
-function setStatus(st) {    
-    switch(st) {
+function eventFire(evt) {
+    dbg = evt;
+    var st = null;
+    switch (evt) {
+        case 'play':
         case 'loadstart':
-            $('#controls').removeClass().addClass('loading');
-            $('#controls').removeClass().addClass('loading');
-        break;
+        case 'waiting':
+        case 'stalled':
+            if (status != 'loading') {
+                st = 'loading';
+            }
+            break;
         case 'playing':
-            $('#controls').removeClass().addClass('playing');
-        break;
         case 'pause':
-            $('#controls').removeClass().addClass('pause');
-        break;
-        default: 
-            $('#controls').removeClass().addClass('error');
-            $('#controls > .station').css('background-image','url(img/err.png)');
+        case 'error': // do this in case error: $('#controls').removeClass().addClass('error');
+            st = evt;
     }
-    if(st) {
+    if (st) {
+        dbg += ' > ' + st;
         status = st;
+        $('#controls').removeClass().addClass(st);
     }
-    
+    //console.log(dbg);
 }
